c48fd4e4e80e8afa335015c2f94629b352fbcd89
dkirkley <dkirkley@gmail.com>
2018-06-05 15:39:29 -0700

    Updated readme and fixed typo

--- Gather-CellData.ps1
+++ Gather-CellData.ps1
@@ -4,7 +4,7 @@
 This PowerShell script populates environment variables with mobile broadband information.
 
 .DESCRIPTION
-Iff the device has a mobile broadband modem, mobile broadband information (SIM, phone, ID, signal, model, firmware)
+If the device has a mobile broadband modem, mobile broadband information (SIM, phone, ID, signal, model, firmware)
 is collected and stored in environment variables. These variables are then collected via SCCM (or other means). 
 The '-logging' parameter may be used to enable logging. The '-inventory' parameter may be used to force a Delta or Full SCCM hardware inventory.
 
